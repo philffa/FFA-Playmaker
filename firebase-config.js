a41a10b569ba443f2e963e0001e53f4c1eb3dfee
philffa <philkip_j@hotmail.com>
2026-03-23 20:28:51 +1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
-// ============================================================
-//  REPLACE THESE VALUES WITH YOUR FIREBASE PROJECT CONFIG
-//  Firebase Console → Project Settings → Your apps → SDK setup
-// ============================================================
-const FIREBASE_CONFIG = {
-  apiKey:            "YOUR_API_KEY",
-  authDomain:        "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId:         "YOUR_PROJECT_ID",
-  storageBucket:     "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId:             "YOUR_APP_ID"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCzvBIS73Fno-7pa09O9Et_XR2pyuUD0zw",
+  authDomain: "ffa-playmaker.firebaseapp.com",
+  databaseURL: "https://ffa-playmaker-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ffa-playmaker",
+  storageBucket: "ffa-playmaker.firebasestorage.app",
+  messagingSenderId: "933713794922",
+  appId: "1:933713794922:web:72fec8fa4cf21a57173713",
+  measurementId: "G-KJ7YNXJGVY"
 };
